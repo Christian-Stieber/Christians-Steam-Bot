5b9284ad396580d36a66c26db63658c903c5dc2c
Christian Stieber <stieber.chr@gmail.com>
2024-06-02 13:09:02 +0200

    Added come cpack support, and removed the MakeDebianPackage script

---
+++ CPack.cmake
@@ -0,0 +1,6 @@
+set(CPACK_PACKAGE_VENDOR "Christian Stieber")
+set(CPACK_PACKAGE_DESCRIPTION_SUMMARY "A work-in-progress tool to interact with Steam")
+set(CPACK_RESOURCE_FILE_LICENSE "${CMAKE_CURRENT_SOURCE_DIR}/LICENSE")
+set(CPACK_PACKAGE_CONTACT "Christian Stieber <stieber.chr@gmail.com>")
+
+set(CPACK_DEBIAN_PACKAGE_SECTION "misc")
